42e3ea488f0352072029087445c9fc0ab5854243
Your Name <you@example.com>
2022-10-07 17:10:01 -0300

    modal

--- java/teste.js
+++ java/teste.js
@@ -9,3 +9,15 @@ var exib = document.getElementById('exib');
 // inserindo o elemento na paǵina
 exib.appendChild(elementoClone);
 
+function showModal(idModal) {
+    const modal = document.querySelector(idModal)
+    modal.style.display = 'flex'  
+}
+
+function hideModal(idModal, event) {
+    if (event.target.className === 'modal') {
+        const modal = document.querySelector(idModal)
+         modal.style.display='none'
+    }
+   
+}
