9d61c18c99957fbef7a566f304cae34cce561e79
Your Name <you@example.com>
2022-11-09 14:46:18 -0300

    modal

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+function showModal(idModal){ 
+    //idModal é usado para avisar que há um parâmetro lá no html, o texto pode ser qualquer coisa para avisar que o parâmetro será um texto.
+    const modal = document.querySelector(idModal)
+    modal.style.display = "flex"
+}
+
+function hideModal(idModal, event) {
+    if(event.target.className === 'modal'){
+        const modal = document.querySelector(idModal)
+        modal.style.display = 'none'
+    }
+}
+
+//forçar o fechamento após receber a mensagem de alerta
+function closeAllModal() {
+    const modais = document.querySelectorAll('.modal')
+    modais.forEach(modal => {
+        modal.style.display = 'none'
+    })
+}
